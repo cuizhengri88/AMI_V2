662e0a360efc64df67e6baf34b3bc7af02621e4d
cuizhengri <cuizhengri@aspnc.com>
2026-03-05 15:16:05 +0800

    빌드 환경 구성

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,8 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+// src-tauri/src/main.rs
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
